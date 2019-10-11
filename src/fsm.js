4e435776a49c780e8af6464f335db157c9c50e8e
Dzmitry Malakhouski <dzmitry.malakhouski@tut.by>
2019-10-11 15:46:40 +0300

    feat: task solved

--- src/fsm.js
+++ src/fsm.js
@@ -1,32 +1,74 @@
+
 class FSM {
     /**
      * Creates new FSM instance.
      * @param config
      */
-    constructor(config) {}
+    constructor(config) {
+        this.initial = config.initial;
+        this.states = config.states;
+        this.currentState = "normal";
+        this.nextState = null;
+        this.prevState = null;
+        this.statesStack = [this.initial];
+        this.pointer = 0; 
+    }
 
     /**
      * Returns active state.
      * @returns {String}
      */
-    getState() {}
-
+    getState() {
+        return this.currentState;
+    }
+    
     /**
      * Goes to specified state.
      * @param state
      */
-    changeState(state) {}
+    changeState(state) {
+        if(state in this.states){
+            this.prevState = this.currentState;
+            this.currentState = state;
+            this.statesStack.splice(this.pointer, this.statesStack.length-1-this.pointer, this.currentState);
+            this.pointer++ ;
+            return state;
+        }
+        else {
+            throw new Error('Error');
+        }
+        
+    }
 
     /**
      * Changes state according to event transition rules.
      * @param event
      */
-    trigger(event) {}
+    trigger(event) {
+        if(event in this.states[this.currentState].transitions){
+            this.prevState = this.currentState;
+            this.currentState = this.states[this.currentState].transitions[event];
+            this.pointer++;
+            this.statesStack.splice(this.pointer, this.statesStack.length-1-this.pointer , this.currentState);
+            
+        }
+        else {
+            throw new Error('Error');
+        }
+        
+
+        
+
+
+    }
 
     /**
      * Resets FSM state to initial.
      */
-    reset() {}
+    reset() {
+        this.changeState(this.initial);
+        this.pointer = 0;
+    }
 
     /**
      * Returns an array of states for which there are specified event transition rules.
@@ -34,28 +76,75 @@ class FSM {
      * @param event
      * @returns {Array}
      */
-    getStates(event) {}
+    getStates(event) {
+        let ArrayOfStates = [];
+        if(event == undefined) {
+            return Object.keys(this.states);
+        } else {
+            Object.keys(this.states).forEach(element => {
+                if(event in this.states[element].transitions) { 
+                    ArrayOfStates.push(element);
+                }  
+            }
+            );
+            return ArrayOfStates; 
+        }
+       
+    }
 
     /**
      * Goes back to previous state.
      * Returns false if undo is not available.
      * @returns {Boolean}
      */
-    undo() {}
+    undo() {
+        if (this.pointer == 0){
+            return false;
+        } else {
+            this.nextState = this.statesStack[this.pointer];
+            this.currentState = this.prevState;
+            this.pointer-- ;
+            if(this.pointer == 0){ 
+                this.prevState = null;
+            } else {            
+                this.prevState = this.statesStack[this.pointer];
+            }
+            
+            
+            
+            return true;
+        }
+    }
 
     /**
      * Goes redo to state.
      * Returns false if redo is not available.
      * @returns {Boolean}
      */
-    redo() {}
+    redo() {
+        if (this.pointer == this.statesStack.length-1){
+            return false;
+        } else {
+            this.prevState = this.statesStack[this.pointer];
+            this.currentState = this.nextState;
+            this.pointer++ ;
+            this.nextState = this.statesStack[this.pointer+1];  
+            return true;
+        }  
+    }
 
     /**
      * Clears transition history
      */
-    clearHistory() {}
+    clearHistory() {
+       this.currentState = "normal";
+       this.nextState = null;
+       this.prevState = null;
+       this.pointer = 0;
+       this.statesStack = [this.initial]; 
+    }
 }
-
+ 
 module.exports = FSM;
 
 /** @Created by Uladzimir Halushka **/
